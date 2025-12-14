181ce58ccaf7a9efaa2804277e5cca6ccdded76a
albaceo12 <h.nouri1999@gmail.com>
2025-12-15 00:07:04 +0330

    fix: cleanup next config and add trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
